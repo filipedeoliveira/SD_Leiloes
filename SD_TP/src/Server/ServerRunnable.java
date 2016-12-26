ed3df0d06f6dc957b8b1062e045dd8a71c91bed4
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2016-12-26 14:17:09 +0000

    Comandos

--- SD_TP/src/Server/ServerRunnable.java
+++ SD_TP/src/Server/ServerRunnable.java
@@ -8,6 +8,8 @@ package Server;
 import Data.Cliente;
 import Data.Clientes;
 import Data.Licitacoes;
+import exceptions.UtilizadorNaoExisteException;
+import exceptions.UtilizadorOfflineException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,25 +27,52 @@ public class ServerRunnable implements Runnable {
     private Cliente cliente;
     private PrintWriter outputServer;
     private BufferedReader inputClient;
-    
+
     private Clientes clientes;
     private Licitacoes licitacoes;
 
-    public ServerRunnable(Socket cliente, Clientes clientes, Licitacoes licitacoes) throws IOException{
-        this.nome=null;
-        this.clienteSock=cliente;
-        this.cliente=null;
-        this.clientes=clientes;
-        this.licitacoes=licitacoes;
+    public ServerRunnable(Socket cliente, Clientes clientes, Licitacoes licitacoes) throws IOException {
+        this.nome = null;
+        this.clienteSock = cliente;
+        this.cliente = null;
+        this.clientes = clientes;
+        this.licitacoes = licitacoes;
         this.outputServer = new PrintWriter(clienteSock.getOutputStream());
         this.inputClient = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-        
+
     }
-    
-    
+
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+            String mensage;
+            this.outputServer = new PrintWriter(clienteSock.getOutputStream());
+            this.inputClient = new BufferedReader(new InputStreamReader(clienteSock.getInputStream()));
+
+            while ((mensage = inputClient.readLine()) != null) {
+                System.out.println("->" + clienteSock.getPort() + mensage);
+                this.comandos(mensage);
+            }
+
+        } catch (IOException e) {
+            System.out.println("O cliente " + clienteSock.getPort() + "saiu do sistema!");
+            System.out.println("Logout executado! Até breve!");
+            if (this.nome != null) {
+                try {
+                    clientes.logOut(this.nome);
+                } catch (UtilizadorNaoExisteException | UtilizadorOfflineException ex1) {
+                }
+            }
+
+        } finally {
+            outputServer.close();
+        }
+
     }
-    
+
+    public void comandos(String comando) {
+
+    }
+
 }
